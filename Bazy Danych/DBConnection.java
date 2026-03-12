2d03bbcebab4febfa50b8dc2ab5ee0e9ddda9966
whylosss <166538534+whylosss@users.noreply.github.com>
2026-03-12 13:43:20 +0100

    Add files via upload

---
+++ Bazy Danych/DBConnection.java
@@ -0,0 +1,11 @@
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/biblioteka";
+    private static final String USER = "root";
+    private static final String PASS = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
